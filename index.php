c2eb7cbd9406c97b5f4e42bbf7f30a1888985d21
Vlatko <krstik1212@gmail.com>
2020-05-16 16:56:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Bouffage</title>
+    </head>
+<body>
+    <h1>This is a recipe web page</h1>
+    <?php
+        print "It is what it is."
+    ?>
+</body>
+</html>
